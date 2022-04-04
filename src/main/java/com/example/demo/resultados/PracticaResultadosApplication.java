053e643847bfa2b8e1f9a4e4698bbeb7954659f1
Tomas <tomasbudassi@gmail.com>
2022-04-04 16:53:20 -0300

    first commit

---
+++ src/main/java/com/example/demo/resultados/PracticaResultadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.resultados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaResultadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaResultadosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/resultados/controllers/resultados.java
@@ -0,0 +1,15 @@
+package com.example.demo.resultados.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class resultados {	
+	
+	@GetMapping("/resultado/{resultado}")
+	public String resultado(@PathVariable  String resultado) {
+		
+		return "El resultado es: " + resultado;
+	}
+}
